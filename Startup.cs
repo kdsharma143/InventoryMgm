8b8442812e092f10c2d4750922caad06da910932
Kuldeep Sharma <kdsharma24@yahoo.com>
2021-04-17 17:36:00 +0530

    Added project

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,17 @@
+using BusinessAccess;
+using BusinessAccess.Interfaces;
+using BusinessAccess.Services;
+using DataAccess.DataModel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using ThinkBridgeAssignment.Middlewares;
 
 namespace ThinkBridgeAssignment
 {
@@ -26,15 +28,42 @@ namespace ThinkBridgeAssignment
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().SetCompatibilityVersion(CompatibilityVersion.Latest).ConfigureApiBehaviorOptions(opions =>
+            {
+                opions.InvalidModelStateResponseFactory = (ActionContext) =>
+                  {
+                      var errors = ActionContext.ModelState.Values.SelectMany(x => x.Errors.Select(p => new
+                      {
+                          errorMessage = p.ErrorMessage
+                      })).ToList();
+
+                      var result = new ApiResponse
+                      {
+                          Status = 0,
+                          Data = errors,
+                          Message = "Validation Failed",
+
+                      };
+                      return new BadRequestObjectResult(result);
+                  };
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ThinkBridgeAssignment", Version = "v1" });
             });
+
+            
+            var ConnectionString = Configuration.GetConnectionString("ThinkInvDb");
+
+            services.AddDbContext<ModelContext>(options => options.UseSqlServer(ConnectionString, b => b.MigrationsAssembly("ThinkBridgeAssignment")));
+
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<ITransactionService, TransactionService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ModelContext db)
         {
             if (env.IsDevelopment())
             {
@@ -43,8 +72,9 @@ namespace ThinkBridgeAssignment
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ThinkBridgeAssignment v1"));
             }
 
+            db.Database.EnsureCreated();
             app.UseRouting();
-
+            app.UseMiddleware(typeof(ErrorMiddleware));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
